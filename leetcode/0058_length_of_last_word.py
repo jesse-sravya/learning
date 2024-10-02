57e590fe6c04d6527ae7aba062d2f6a8739a8f8c
Jesse Sravya <jesse.dhara@gmail.com>
2024-10-02 20:28:32 +0530

    solution for leetcode 58

---
+++ leetcode/0058_length_of_last_word.py
@@ -0,0 +1,11 @@
+from test_helper import TestHelper
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
+
+TestHelper.test(Solution().lengthOfLastWord("   fly me   to   the moon  "), 4)
+TestHelper.test(Solution().lengthOfLastWord("   fly me   to   the moo"), 3)
+TestHelper.test(Solution().lengthOfLastWord("m"), 1)
+TestHelper.test(Solution().lengthOfLastWord("mmm mmm"), 3)
